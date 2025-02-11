e83a391f77eab348c80a722765127a8ba5a46b8a
avelar1x <valmir.cesai@gmail.com>
2025-02-11 19:55:31 -0300

    att
    att

---
+++ aula32/index.js
@@ -0,0 +1,10 @@
+// For in
+const frutas = ['Manga', 'Uva', 'Pera', 'Maça']
+
+for ( let i = 0; i < frutas.length; i++){
+    console.log(frutas[i])
+}
+
+for (let f in frutas){
+    console.log(f)
+}
---
+++ aula32/node_93345b674ab66.tmp.js
@@ -0,0 +1,6 @@
+// For in
+const frutas = ['Manga', 'Uva', 'Pera', 'Maça']
+
+for ( let i = 0; i < frutas.length; i++){
+    console.log("", frutas[])
+}
