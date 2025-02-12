8b14605b48c79cac4c0dfdcb480c5219affbb84d
avelar1x <valmir.cesai@gmail.com>
2025-02-12 19:15:41 -0300

    att
    att

--- aula32/index.js
+++ aula32/index.js
@@ -5,6 +5,20 @@ for ( let i = 0; i < frutas.length; i++){
     console.log(frutas[i])
 }
 
-for (let f in frutas){
-    console.log(f)
+console.log("-")
+
+for (let indice in frutas){
+    console.log(frutas[indice])
+}
+
+console.log("-")
+
+const pessoa = {
+    nome: "Valmir",
+    sobrenome: "Avelar",
+    idade: 19 
+}
+
+for (let p in pessoa){
+    console.log (p, pessoa [p])
 }
