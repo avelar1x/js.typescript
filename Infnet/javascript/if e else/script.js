796aca40d93a2586e1c1e4cea918999373fc489d
avelar1x <valmir.cesai@gmail.com>
2025-03-06 22:28:13 -0300

    att
    att

--- Infnet/javascript/if e else/script.js
+++ Infnet/javascript/if e else/script.js
@@ -1,4 +1,4 @@
-const idade = Number(prompt('Digite a sua idade'))
+/*const idade = Number(prompt('Digite a sua idade'))
 
 if (idade >= 18){
     alert("Pode entrar")
@@ -21,4 +21,68 @@ if (resposta){
     alert('Você é legal')
 } else {
     alert("Você não é legal")
+}
+*/
+/* let a;
+let b;
+let c;
+let d;
+let e;
+
+// Comentar SOMENTE as linhas abaixo
+a = 3;
+//a = 2;
+//a = 7;
+//a = 0;
+
+b = undefined;
+//b = null;
+//b = 1;
+//b = "null";
+
+//c = "2000" + "20";
+c = /*"20"*/ +20;
+//c = null;
+//c = undefined;
+/*
+//d = null;
+d = undefined;
+//d = 0;
+//d = "undefined";
+
+e = null;
+//e = 0;
+//e = NaN;
+//e = "null";
+// Comentar SOMENTE as linhas acima
+
+const verificacaoA = a > 2 && a < 4;
+const verificacaoB = b === undefined;
+const verificacaoC = typeof c === "number";
+const verificacaoD = d == undefined;
+const verificacaoE = e === d;
+
+console.log("a=" + verificacaoA);
+console.log("b=" + verificacaoB);
+console.log("c=" + verificacaoC);
+console.log("d=" + verificacaoD);
+console.log("e=" + verificacaoE);
+*/
+
+
+let escolha = prompt("Escolha entre: Pedra, Papel ou Tesoura").toLowerCase();
+let opcoes = ["Pedra", "Papel", "Tesoura"];
+let escolhaBot = opcoes[Math.floor(Math.random() * opcoes.length)];
+alert("O bot escolheu: " + escolhaBot);
+
+if (escolha === escolhaBot) {
+  alert("Empate!");
+} else if (
+  (escolhaUsuario === "pedra" && escolhaComputador === "tesoura") ||
+  (escolhaUsuario === "papel" && escolhaComputador === "pedra") ||
+  (escolhaUsuario === "tesoura" && escolhaComputador === "papel")
+) {
+  alert("Você ganhou!");
+} else {
+  alert("O computador ganhou!");
 }
---
+++ aula35/index.js
@@ -0,0 +1,12 @@
+function soma(x, y){
+    if (typeof x !== "number" || typeof y !== "number"){
+        throw new ReferenceError("x e y precisam ser números");
+    }
+
+    return x + y;
+}
+try{
+    console.log(soma('1', 2));
+} catch (err){
+    console.log(err)
+}
