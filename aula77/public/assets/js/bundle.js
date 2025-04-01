3165cd5416758192505046f688c78bc3595be953
avelar1x <valmir.cesai@gmail.com>
2025-03-31 22:26:19 -0300

    att
    att

---
+++ aula77/public/assets/js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+function criaPessoa(nome, sobrenome) {
+  nome = nome;
+  sobrenome = sobrenome;
+}
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
---
+++ aula77/src/index.js
@@ -0,0 +1,4 @@
+function criaPessoa(nome, sobrenome){
+    nome = nome
+    sobrenome = sobrenome
+}
